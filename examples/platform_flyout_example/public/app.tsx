b0db34b0931eab5384779b308e67982c936130c3
Timothy Sullivan <tsullivan@elastic.co>
2025-05-19 15:48:18 -0700

    flyout/experiements

---
+++ examples/platform_flyout_example/public/app.tsx
@@ -0,0 +1,52 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import { EuiPageHeader, EuiPageSection, EuiSpacer, EuiTab, EuiTabs } from '@elastic/eui';
+import { AppMountParameters, CoreStart } from '@kbn/core/public';
+import { KibanaRenderContextProvider } from '@kbn/react-kibana-context-render';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { GreyboxExample } from './examples/greybox_example';
+import { StartDeps } from './plugin';
+
+const App = ({
+  core,
+  deps,
+  mountParams,
+}: {
+  core: CoreStart;
+  deps: StartDeps;
+  mountParams: AppMountParameters;
+}) => {
+  return (
+    <KibanaRenderContextProvider {...core}>
+      <EuiPageHeader
+        paddingSize="l"
+        restrictWidth={true}
+        bottomBorder="extended"
+        pageTitle="Journey flyouts"
+        description="This example app demonstrates how to use the Journey Flyouts API for flyout-to-flyout interactions, and for showing detail content side by side with main content."
+      />
+      <EuiPageSection restrictWidth={false} alignment={'top'} color={'plain'} grow={true}>
+        <EuiTabs>
+          <EuiTab isSelected={true}>Greybox example</EuiTab>
+        </EuiTabs>
+        <EuiSpacer />
+
+        <GreyboxExample />
+      </EuiPageSection>
+    </KibanaRenderContextProvider>
+  );
+};
+
+export const renderApp = (core: CoreStart, deps: StartDeps, mountParams: AppMountParameters) => {
+  ReactDOM.render(<App core={core} deps={deps} mountParams={mountParams} />, mountParams.element);
+
+  return () => ReactDOM.unmountComponentAtNode(mountParams.element);
+};
---
+++ examples/platform_flyout_example/public/journey_flyouts/types.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import { WithAllKeys } from '@kbn/presentation-publishing';
+import { StateManager } from '@kbn/presentation-publishing/state_manager/types';
+
+export interface FlyoutEntry<StateType extends object = {}> {
+  Component: React.FC<FlyoutProps<StateType>>;
+  width: number;
+  initialState?: WithAllKeys<StateType>;
+}
+
+export interface FlyoutProps<ThisFlyoutState extends object = {}> {
+  openNextFlyout: <NextFlyoutState extends object = {}>(
+    entry: FlyoutEntry<NextFlyoutState>
+  ) => void;
+  openChildFlyout: <ChildFlyoutState extends object = {}>(
+    entry: FlyoutEntry<ChildFlyoutState>
+  ) => void;
+  stateManager: StateManager<ThisFlyoutState>;
+}
+
+export interface FlyoutApi {
+  openFlyout: <InitialFlyoutState extends object = {}>(
+    entry: FlyoutEntry<InitialFlyoutState>
+  ) => void;
+}
