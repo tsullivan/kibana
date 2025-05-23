002be651242c555ec9fcd37f9a80adb8baf4fd54
Timothy Sullivan <tsullivan@elastic.co>
2025-05-23 14:31:03 -0700

    Add mock

---
+++ src/core/packages/overlays/browser-mocks/src/use_managed_flyout.mock.ts
@@ -0,0 +1,23 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the "Elastic License
+ * 2.0", the "GNU Affero General Public License v3.0 only", and the "Server Side
+ * Public License v 1"; you may not use this file except in compliance with, at
+ * your election, the "Elastic License 2.0", the "GNU Affero General Public
+ * License v3.0 only", or the "Server Side Public License, v 1".
+ */
+
+import { BehaviorSubject } from 'rxjs';
+import { UseManagedFlyoutApi } from '@kbn/core-overlays-browser';
+
+export const useManagedFlyoutMock = jest.fn<UseManagedFlyoutApi, []>(() => ({
+  openFlyout: jest.fn(),
+  closeFlyout: jest.fn(),
+  isFlyoutOpen: jest.fn(() => false),
+  onFlyoutToggle: new BehaviorSubject(false),
+  nextFlyout: jest.fn(),
+  goBack: jest.fn(),
+  canGoBack: jest.fn(() => false),
+  openChildFlyout: jest.fn(),
+  closeChildFlyout: jest.fn(),
+}));
